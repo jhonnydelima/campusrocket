871ceb9fb12270ed489ba891e01bad8d7f92f44a
Jhonny Lima <jlima@Jhonnys-MacBook-Pro.local>
2021-03-01 01:42:42 -0300

    added routes to show and edit the teachers

--- routes.js
+++ routes.js
@@ -1,23 +1,27 @@
-const express = require('express')
-const teachers = require('./teachers')
-const routes = express.Router()
+const express = require('express');
+const teachers = require('./teachers');
+const routes = express.Router();
 
+// TEACHERS
 routes.get('/', (req, res) => {
-  return res.redirect("/teachers")
-})
+  return res.redirect("/teachers");
+});
 
 routes.get('/teachers', (req, res) => {
-  return res.render("teachers/index")
-})
+  return res.render("teachers/index");
+});
 
 routes.get('/teachers/create', (req, res) => {
-  return res.render("teachers/create")
-})
+  return res.render("teachers/create");
+});
 
-routes.post('/teachers', teachers.post)
+routes.post('/teachers', teachers.post);
+routes.get('/teachers/:id', teachers.show);
+routes.get('/teachers/:id/edit', teachers.edit);
 
+// STUDENTS
 routes.get('/students', (req, res) => {
-  return res.send("students")
-})
+  return res.send("students");
+});
 
-module.exports = routes
+module.exports = routes;
--- teachers.js
+++ teachers.js
@@ -1,5 +1,6 @@
-const fs = require('fs')
-const data = require('./data.json')
+const fs = require('fs');
+const data = require('./data.json');
+const { age, graduation, classType, date } = require('./utils');
 
 // create
 exports.post = (req, res) => {
@@ -28,9 +29,53 @@ exports.post = (req, res) => {
         created_at
     })
 
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
         if (err) return res.send("Write file error!")
 
         return res.redirect("/teachers")
     })
+}
+
+// show
+exports.show = (req, res) => {
+    const { id } = req.params;
+
+    const foundTeacher = data.teachers.find(teacher => {
+        return teacher.id == id;
+    });
+
+    if (!foundTeacher) {
+        return res.send("Teacher not found!");
+    }
+
+    const teacher = {
+        ...foundTeacher,
+        age: age(foundTeacher.birth),
+        graduation: graduation(foundTeacher.educational_level),
+        class_type: classType(foundTeacher.class_type),
+        occupation_area: foundTeacher.occupation_area.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundTeacher.created_at),
+    };
+
+    return res.render('teachers/show', { teacher });
+}
+
+// edit
+exports.edit = (req, res) => {
+    const { id } = req.params;
+
+    const foundTeacher = data.teachers.find(teacher => {
+        return teacher.id == id;
+    });
+
+    if (!foundTeacher) {
+        return res.send("Teacher not found!");
+    }
+
+    const teacher = {
+        ...foundTeacher,
+        birth: date(foundTeacher.birth),
+    }
+
+    return res.render('teachers/edit', { teacher });
 }
